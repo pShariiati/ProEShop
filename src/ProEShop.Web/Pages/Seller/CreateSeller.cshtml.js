03d0d8ec6064619fad46a97bd373c8a4669d78e3
Payam Shariati <pshariiati@gmail.com>
2022-01-25 17:17:26 +0330

    E-71 ... Next and previous buttons

--- src/ProEShop.Web/Pages/Seller/CreateSeller.cshtml.js
+++ src/ProEShop.Web/Pages/Seller/CreateSeller.cshtml.js
@@ -34,3 +34,41 @@ function addRequiredRule(selector) {
         }
     });
 }
+
+var firstTab = $('#create-seller-container .nav-tabs button:first').attr('data-bs-target');
+var lastTab = $('#create-seller-container .nav-tabs button:last').attr('data-bs-target');
+
+$('#create-seller-container #previous-tab-create-seller').attr('disabled', 'disabled');
+var currentTab = $('#create-seller-container .nav-tabs button:first').attr('data-bs-target');
+
+$('#create-seller-container #next-tab-create-seller').click(function () {
+    var nextTab = $(`#create-seller-container .nav-tabs button[data-bs-target="${currentTab}"]`).next();
+    if (nextTab.attr('data-bs-target')) {
+        currentTab = nextTab.attr('data-bs-target');
+        nextTab.tab('show');
+    }
+});
+
+$('#create-seller-container #previous-tab-create-seller').click(function () {
+    var prevTab = $(`#create-seller-container .nav-tabs button[data-bs-target="${currentTab}"]`).prev();
+    if (prevTab.attr('data-bs-target')) {
+        currentTab = prevTab.attr('data-bs-target');
+        prevTab.tab('show');
+    }
+});
+
+$('#create-seller-container .nav-tabs button').on('show.bs.tab', function (e) {
+    currentTab = $(e.target).attr('data-bs-target');
+    if (currentTab == lastTab) {
+        $('#create-seller-container #next-tab-create-seller').attr('disabled', 'disabled');
+    }
+    else {
+        $('#create-seller-container #next-tab-create-seller').removeAttr('disabled');
+    }
+    if (currentTab == firstTab) {
+        $('#create-seller-container #previous-tab-create-seller').attr('disabled', 'disabled');
+    }
+    else {
+        $('#create-seller-container #previous-tab-create-seller').removeAttr('disabled');
+    }
+});
