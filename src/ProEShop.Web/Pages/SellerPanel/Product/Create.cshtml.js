7dc765505620ecff7dcaabe77f571564101fdae4
Payam Shariati <pshariiati@gmail.com>
2022-02-26 22:23:02 +0330

    E-89 ... Continuing select product category

--- src/ProEShop.Web/Pages/SellerPanel/Product/Create.cshtml.js
+++ src/ProEShop.Web/Pages/SellerPanel/Product/Create.cshtml.js
@@ -9,9 +9,24 @@ var selectedCategoriesIds = [];
 
 function showCategories(data) {
     $('#product-category div.row.card-body').html(data);
+    selectedCategoriesIds.forEach(element => {
+        $('#product-category button[category-id=' + element + ']').addClass('active');
+    });
     $('#product-category div.row.card-body button[has-child=true]').click(function () {
-        var selectedCategoryId = $(this).attr('category-id');
-        selectedCategoriesIds.push(selectedCategoryId);
+        var selectedRow = parseInt($(this).parent().attr('category-row'));
+        debugger;
+        for (var counter = selectedRow; counter <= selectedCategoriesIds.length; counter++) {
+            debugger;
+            $('#product-category div[category-row=' + counter + '] button').removeClass('active');
+        }
+        $(this).addClass('active');
+        selectedCategoriesIds = [];
+        $('#product-category button.active').each(function () {
+            debugger;
+            var a = $(this).attr('category-id');
+            console.log(a);
+            selectedCategoriesIds.push(a);
+        });
         getHtmlWithAJAX(`${location.pathname}?handler=GetCategories`, { selectedCategoriesIds: selectedCategoriesIds }, 'showCategories', null);
     });
 }
