b4a5cd33ada966db8e8503659b7cd1edb6bb9390
Payam Shariati <pshariiati@gmail.com>
2022-07-23 07:41:24 +0430

    E-160 ... Continuing product variants in single page of product

--- src/ProEShop.Web/Pages/Product/Index.cshtml.js
+++ src/ProEShop.Web/Pages/Product/Index.cshtml.js
@@ -9,4 +9,24 @@
 
     // Add check to the first variant
     $('#product-variants-box-in-show-product-info i:first').removeClass('d-none');
+
+    $('#product-variants-box-in-show-product-info div').click(function() {
+        $('#product-variants-box-in-show-product-info div').removeClass('selected-variant-in-show-product-info');
+        $('#product-variants-box-in-show-product-info i').addClass('d-none');
+
+        $(this).find('i').removeClass('d-none');
+        $(this).addClass('selected-variant-in-show-product-info');
+
+        var selectedVariantValue = $(this).attr('data-bs-original-title');
+
+        $('.other-sellers-table').addClass('d-none');
+
+        $('.other-sellers-table[variant-value="' + selectedVariantValue + '"]').removeClass('d-none');
+
+        if ($('.other-sellers-table[variant-value="' + selectedVariantValue + '"]').length === 0) {
+            $('#other-sellers-box').addClass('d-none');
+        } else {
+            $('#other-sellers-box').removeClass('d-none');
+        }
+    });
 });
